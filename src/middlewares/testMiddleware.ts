d64e02e76b6451b8dfa2e329604d074661f66ade
Durmuş <demirtasdurmus@gmail.com>
2023-06-21 07:02:54 +0300

    fix(project): format all project with prettier

--- src/middlewares/testMiddleware.ts
+++ src/middlewares/testMiddleware.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 
 export const testMiddleware: RequestHandler = (req, res, next) => {
+    // eslint-disable-next-line no-console
     console.log('I am a middleware!');
     next();
 };
