e739e16a0d5c8f1c656c4e9018a48036463b5e24
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2025-12-26 18:35:44 +0300

    feat: add http logger middleware

---
+++ src/middleware/http-logger.middleware.ts
@@ -0,0 +1,25 @@
+import { RequestHandler } from 'express';
+import { logger } from '../utils/logger';
+
+export const httpLogger: RequestHandler = (req, res, next) => {
+  const start = Date.now();
+
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    const status = res.statusCode;
+    const method = req.method;
+    const url = req.originalUrl;
+
+    const message = `${method} ${url} - ${status} (${duration}ms)`;
+
+    if (status >= 500) {
+      logger.error({ error: res.locals?.error }, message);
+    } else if (status >= 400) {
+      logger.warn({ error: res.locals?.error }, message);
+    } else {
+      logger.info(message);
+    }
+  });
+
+  next();
+};
