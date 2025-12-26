0ed7e640855c7d2dc8e4fbadc087a50b6edeb999
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2025-12-26 18:17:03 +0300

    feat: add error handling and custom error classes

--- src/controllers/sample.controller.ts
+++ src/controllers/sample.controller.ts
@@ -1,7 +1,19 @@
 import { RequestHandler } from 'express';
+import { ServerResponse } from '../types';
 import { sendGreeting } from '../services/sample.service';
 
-export const sampleController: RequestHandler<unknown, string, unknown, unknown> = (_req, res) => {
+export const sampleController: RequestHandler<
+  unknown,
+  ServerResponse<{ message: string }>,
+  unknown,
+  unknown
+> = (_req, res) => {
   const greeting = sendGreeting();
-  res.status(200).send(greeting);
+
+  res.status(200).json({
+    success: true,
+    payload: {
+      message: greeting,
+    },
+  });
 };
---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export type ServerResponse<T extends Record<string, unknown> = Record<string, unknown>> =
+  | {
+      success: true;
+      payload: T | null;
+    }
+  | {
+      success: false;
+      error: T;
+    };
+
+export type BaseErrorIssue = {
+  field?: string;
+  detail?: string;
+};
+
+export type BaseErrorData = {
+  issues?: BaseErrorIssue[];
+  stack?: string;
+  originalError?: unknown;
+};
+
+export type ErrorResponseDetails = {
+  name: string;
+  statusCode: number;
+  message: string;
+  issues?: BaseErrorData['issues'];
+  stack?: BaseErrorData['stack'];
+  originalError?: BaseErrorData['originalError'];
+};
