1a7e314953505a5bad3d9ccb409e8606d5e482a8
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2025-12-27 19:22:13 +0300

    chore: disable logger on test env

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,24 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { env } from '../env';
+
 /**
  * A thin layer over console methods,
  * Replace with a more robust logger like winston or pino in production.
  */
 class Logger {
+  constructor(private enabled: boolean = true) {}
+
   log(message?: any, ...optionalParams: any[]) {
-    console.log(message, ...optionalParams);
+    if (this.enabled) {
+      console.log(message, ...optionalParams);
+    }
   }
 
   error(message?: any, ...optionalParams: any[]) {
-    console.error(message, ...optionalParams);
+    if (this.enabled) {
+      console.error(message, ...optionalParams);
+    }
   }
 
   warn(message?: any, ...optionalParams: any[]) {
-    console.warn(message, ...optionalParams);
+    if (this.enabled) {
+      console.warn(message, ...optionalParams);
+    }
   }
 
   info(message?: any, ...optionalParams: any[]) {
-    console.info(message, ...optionalParams);
+    if (this.enabled) {
+      console.info(message, ...optionalParams);
+    }
   }
 }
 
-export const logger = new Logger();
+export const logger = new Logger(env.NODE_ENV !== 'test');
