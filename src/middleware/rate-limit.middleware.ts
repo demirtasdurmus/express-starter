9cc8ad1e6f7baabb40926dcccf14b064b93ce11b
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2026-05-27 20:16:11 +0300

    fix: rate limiting ip resolving when cf proxy is enabled

--- src/middleware/rate-limit.middleware.ts
+++ src/middleware/rate-limit.middleware.ts
@@ -1,5 +1,5 @@
 import { ParseKeys } from 'i18next';
-import expressRateLimit from 'express-rate-limit';
+import expressRateLimit, { ipKeyGenerator } from 'express-rate-limit';
 import { TooManyRequestsError } from '../lib/error';
 import { apiConfig } from '../config';
 
@@ -7,6 +7,26 @@ export const rateLimit = expressRateLimit({
   ...apiConfig.apiRateLimit,
   standardHeaders: true,
   legacyHeaders: false,
+
+  keyGenerator: (req) => {
+    const cfConnectingIp = req.headers['cf-connecting-ip'];
+
+    if (
+      apiConfig.trustedCloudflareProxy &&
+      typeof cfConnectingIp === 'string' &&
+      cfConnectingIp.trim()
+    ) {
+      return ipKeyGenerator(cfConnectingIp);
+    }
+
+    const fallbackIp = req.ip || req.socket.remoteAddress;
+
+    if (fallbackIp) {
+      return ipKeyGenerator(fallbackIp);
+    }
+
+    return 'unknown'; // Fallback key if no IP can be determined
+  },
   handler: (_req, _res, _next) => {
     throw new TooManyRequestsError('samples.toManyRequests' satisfies ParseKeys);
   },
